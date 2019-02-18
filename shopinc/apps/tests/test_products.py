e4086ba282311783cfe99b08e446b70dee92f559
James Macharia <jamesmasher13@gmail.com>
2019-02-18 20:09:44 +0300

    Add one test

---
+++ shopinc/apps/tests/test_products.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from shopinc.apps.product.models import Product
+
+
+class ModelProductTest(TestCase):
+    def setUp(self):
+        self.product = Product(
+            name="name", slug="name-123456d", description="asdfgh", price=1234,
+            added_at="2019-2-19", updated_at="2019-4-12", image_url="sdfgh"
+        )
+
+    def test_model_can_create_a_product(self):
+        """Test the bucketlist model can create a bucketlist."""
+        old_count = Product.objects.count()
+        self.product.save()
+        new_count = Product.objects.count()
+        self.assertNotEqual(old_count, new_count)
