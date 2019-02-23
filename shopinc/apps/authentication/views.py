a2595a8a0ea05e90afc522357f9af305bc3f1f2f
James Macharia <jamesmasher13@gmail.com>
2019-02-23 13:04:17 +0300

    Add Email conf

--- shopinc/apps/authentication/views.py
+++ shopinc/apps/authentication/views.py
@@ -1,4 +1,6 @@
 
+from django.conf import settings
+from django.core.mail import send_mail
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import (
@@ -22,7 +24,21 @@ class RegistrationAPIView(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
+        # RegistrationAPIView.send_email(user)
+
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @staticmethod
+    def send_email(user):
+        try:
+            subject = "Thanks for using chosing shopinc"
+            body = "Shop at your pace and get the best products"
+            recipient = user['email']
+            sender = settings.EMAIL_HOST_USER
+            send_mail(subject, body, sender, [
+                recipient], fail_silently=True)
+        except:
+            print("failed")
 
 
 class LoginAPIView(generics.CreateAPIView):
--- shopinc/settings.py
+++ shopinc/settings.py
@@ -133,3 +133,10 @@ AUTH_USER_MODEL = 'authentication.User'
 REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
