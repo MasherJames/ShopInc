47ff5f76a0ce0cbc3ddf50d4dbd4a2e160623021
James Macharia <jamesmasher13@gmail.com>
2019-04-10 11:26:42 +0300

    Add cors

--- shopinc/settings.py
+++ shopinc/settings.py
@@ -116,8 +116,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3030',
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
